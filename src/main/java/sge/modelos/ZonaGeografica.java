be089f358806817c9158f1cd02e8d84cd71f38e1
Gonzalo Guini <gonzalo2014@gmail.com>
2018-07-24 23:21:25 -0300

    subo clase zona geografica, falta desarrollo

---
+++ src/main/java/sge/modelos/ZonaGeografica.java
@@ -0,0 +1,13 @@
+package sge.modelos;
+
+public class ZonaGeografica {
+	
+	private int Id;
+	private String nombre;
+	private float longitudCentro;
+	private float latitudCentro;
+	private int radio;
+	
+	//una #ZonaGeografica encierra uno o mas #Transformadores
+
+}
