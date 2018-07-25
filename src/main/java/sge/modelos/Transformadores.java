dd5edba6d36c25d090ec62d1ae91e8968d75f5f4
Gonzalo Guini <gonzalo2014@gmail.com>
2018-07-25 19:04:14 -0300

    subo setters & getters

--- src/main/java/sge/modelos/Transformadores.java
+++ src/main/java/sge/modelos/Transformadores.java
@@ -2,11 +2,25 @@ package sge.modelos;
 
 public class Transformadores {
 
-	private int Id;
+	private int id;
 	private float longitud;
 	private float latitud;
 	
 	//una #Transformador pertenece a #ZonaGeografica
 	private ZonaGeografica miZona;
 	
+	//Constructor
+	public Transformadores(int Id) {
+		this.id = Id;
+	}
+	
+	//Setters & Getters
+	public void setLongitud(float longitud) {
+		this.longitud = longitud;
+	}
+	
+	public void setLatitud(float latitud) {
+		this.latitud = latitud;
+	}
+		
 }
--- src/main/java/sge/modelos/ZonaGeografica.java
+++ src/main/java/sge/modelos/ZonaGeografica.java
@@ -6,7 +6,7 @@ import sge.transformador.Transformador;
 
 public class ZonaGeografica {
 	
-	private int Id;
+	private int id;
 	private String nombre;
 	private float longitudCentro;
 	private float latitudCentro;
@@ -15,5 +15,25 @@ public class ZonaGeografica {
 	//una #ZonaGeografica encierra uno o mas #Transformadores
 	private List<Transformadores> transformadores;
 
+	//Constructor
+	public ZonaGeografica(int Id) {
+		this.id = Id;
+	}
+	
+	//Setters & Getters
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
+	public void setLongitudCentro(float longitudCentro) {
+		this.longitudCentro = longitudCentro;
+	}
+	
+	public void setLatitudCentro(float latitudCentro) {
+		this.latitudCentro = latitudCentro;
+	}
+	
+	public void setRadio(int radio) {
+		this.radio = radio;
+	}
 }
