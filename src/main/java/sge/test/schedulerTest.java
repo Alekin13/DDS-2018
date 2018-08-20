054917a17a2ba963c35e04dceda301def98cde64
Alejandro <alemattioli95@gmail.com>
2018-08-20 15:51:52 -0300

    Scheduler

---
+++ src/main/java/sge/test/schedulerTest.java
@@ -0,0 +1,42 @@
+package sge.test;
+
+import org.junit.Test;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+import sge.simplexSolver.SimplexJob;
+
+public class schedulerTest {
+	 
+	 public static void main(String[] args) {
+		
+		 try {
+	        	Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+				scheduler.start();
+				
+				JobDetail jobDetalle = JobBuilder.newJob(SimplexJob.class).withIdentity("SimplexJob").build();
+
+				Trigger trigger = TriggerBuilder
+						.newTrigger()
+						.withIdentity("SimplexJobTrigger")
+						.withSchedule(
+						    SimpleScheduleBuilder.simpleSchedule()
+							.withIntervalInSeconds(20).repeatForever())
+						.build();
+				
+				scheduler.scheduleJob(jobDetalle,trigger);
+				
+				
+			} catch (SchedulerException e) {
+				e.printStackTrace();
+			}
+	        
+	}
+}
