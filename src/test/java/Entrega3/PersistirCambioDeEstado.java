8d79d58ab0fd8505f0d4bb6c72e1830b36e2d14f
Alejandro <alemattioli95@gmail.com>
2018-10-09 23:52:46 -0300

    commit

--- src/test/java/Entrega3/PersistirCambioDeEstado.java
+++ src/test/java/Entrega3/PersistirCambioDeEstado.java
@@ -1,5 +1,6 @@
 package Entrega3;
 
+import java.text.ParseException;
 import java.time.LocalDateTime;
 
 import org.junit.Test;
@@ -14,32 +15,45 @@ import Usuario.Cliente;
 public class PersistirCambioDeEstado {
 
 	@Test
-	public void casoPrueba1(){
+	public void casoPrueba1() throws ParseException{
 		
 		EntityManagerHelper dbhelper = new EntityManagerHelper();
+
 		DispositivoFactory fabricaDeDispositivos = new DispositivoFactory();
 		DispositivoEstandar unDispositivo = new DispositivoEstandar(); 
-				
-		unDispositivo = fabricaDeDispositivos.tvTubo21();
+		Cliente unCliente = new Cliente("jey", "123456", "Jael", "Duran", "Av. Rivadavia 6000", LocalDateTime.now(), "DNI", 98745632, 45459595, "R1");
+		dbhelper.persistirCliente(unCliente);
 		
-		dbhelper.persistirDispositivo(unDispositivo);
-
-//		System.out.println(unDispositivo.getEstado().getDescripcion());
-//		
-//		unDispositivo.setEstado("Apagado");
-//		
-//		System.out.println(unDispositivo.getEstado().getDescripcion());
-
-//		Cliente unCliente = new Cliente("jey", "123456", "Jael", "Duran", "Av. Rivadavia 6000", LocalDateTime.now(), "DNI", 98745632, 45459595, "R1");
-//		Cliente otroCliente = new Cliente("Alekin", "123456", "Alejandro", "Mattioli", "Av. Rivadavia 5000", LocalDateTime.now(), "DNI", 38993333, 1551515555, "R1");
-//		
-//		unCliente.addDispositivo(fabricaDeDispositivos.aireAcondicionado2200());
-//		unCliente.addDispositivo(fabricaDeDispositivos.pcDeEscritorio());
-//		otroCliente.addDispositivo(fabricaDeDispositivos.microondas());
-//		
-//		dbhelper.persistirCliente(unCliente);
-//		dbhelper.persistirCliente(otroCliente);
+		unCliente.addDispositivo(fabricaDeDispositivos.aireAcondicionado2200());
+		unCliente.addDispositivo(fabricaDeDispositivos.pcDeEscritorio());
+		dbhelper.modificar(unCliente);
 		
+		//dbhelper.cargarEstadosFromJson("src/test/resources/Data/Estados.json");
+		
+		
+		//Cliente otroCliente = new Cliente("Alekin", "123456", "Alejandro", "Mattioli", "Av. Rivadavia 5000", LocalDateTime.now(), "DNI", 38993333, 1551515555, "R1");
+		
+		//otroCliente.addDispositivo(fabricaDeDispositivos.microondas());
+		
+		
+		
+		
+		
+		
+		//dbhelper.persistirDispositivo(unDispositivo);
+		
+		//dbhelper.persistirCliente(otroCliente);
+		
+		
+
+		
+		
+	//	System.out.println(unDispositivo.getEstado().getDescripcion());
+		
+		//unDispositivo.setEstado("Apagado");
+		
+//		System.out.println(unDispositivo.getEstado().getDescripcion());
+
 		
 		
 	}
--- src/test/java/Entrega3/TestCasoDePruebasMinimos.java
+++ src/test/java/Entrega3/TestCasoDePruebasMinimos.java
@@ -1,8 +1,14 @@
 package Entrega3;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
 
 import Helper.EntityManagerHelper;
 import Usuario.Cliente;
@@ -14,13 +20,23 @@ public class TestCasoDePruebasMinimos {
 		
 		EntityManagerHelper dbhelper = new EntityManagerHelper();
 		
+		EntityManager entityManager = PerThreadEntityManagers.getEntityManager();
+		Query query = entityManager.createQuery("FROM Cliente WHERE apellido = :id");
+
 		Cliente alejandro = new Cliente("Alekin", "123456", "Alejandro", "Mattioli", "Av. Rivadavia 5000", LocalDateTime.now(), "DNI", 38993333, 1551515555, "R1");
 		dbhelper.persistirCliente(alejandro);
-		//recuperar Cliente e imprimir
+		
+		System.out.println(alejandro.getLatitud() + " ; " + alejandro.getLongitud());
+		
+		query.setParameter("id", alejandro.getApellido());
+		List list = query.getResultList();
+		
+		Cliente ale2 = (Cliente) list.get((new Long(0)).intValue());
 		alejandro.setLatitud(20.59);
-		alejandro.setLongitud(14.59);
+		alejandro.setLongitud(14.59);		
 		dbhelper.modificar(alejandro);
-		//recuperar Cliente e imprimir
+
+		System.out.println(ale2.getLatitud() + " ; " + ale2.getLongitud());
 		
 	}
 	
