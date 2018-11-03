f553c196f0b218da2c09301adb8997a0d289fb34
Jey <jaeld94@gmail.com>
2018-11-03 15:22:44 -0300

    Configuracion del server y pruebas sencillas

---
+++ src/main/java/Server/Server.java
@@ -0,0 +1,42 @@
+package Server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Dispositivo.Dispositivo;
+import Dispositivo.DispositivoFactory;
+import Dispositivo.DispositivoInteligente;
+import Usuario.Cliente;
+import spark.debug.DebugScreen;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.ModelAndView;
+import spark.Spark;
+
+public class Server {
+	
+	public static void main(String[] args) {
+		spark.debug.DebugScreen.enableDebugScreen();
+		HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+		Spark.port(8080);
+
+		Spark.get("/hello", (req, res) -> "Hello World");
+		
+		System.out.println("Hoooola");
+		
+		Cliente unCliente = new Cliente();
+		List<Dispositivo> dispositivos = new ArrayList<>();
+		DispositivoFactory fabricaDeDispositivos = new DispositivoFactory();
+		DispositivoInteligente dispositivo = fabricaDeDispositivos.aireAcondicionado2200();
+		
+		dispositivos.add(dispositivo);
+		
+		
+		unCliente.setDispositivos(dispositivos);
+		
+		Spark.get("/elUSuario", (req,res) -> {
+			return unCliente.getDispositivos();
+			
+		});
+		
+	}
+}
