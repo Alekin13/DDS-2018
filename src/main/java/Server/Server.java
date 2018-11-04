520551e1dee5443659aa96bd8609b89b75388ba0
Jey <jaeld94@gmail.com>
2018-11-04 01:49:09 -0300

    no funciona lo agregado con Spark, mañana le doy una vuelta, no tocar

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -3,9 +3,14 @@ package Server;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
+
 import Dispositivo.Dispositivo;
 import Dispositivo.DispositivoFactory;
 import Dispositivo.DispositivoInteligente;
+import Helper.EntityManagerHelper;
 import Usuario.Cliente;
 import spark.debug.DebugScreen;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -14,43 +19,50 @@ import spark.Spark;
 
 public class Server {
 	
-	public static void main(String[] args) {
+	public void loginUsuario() {
 		spark.debug.DebugScreen.enableDebugScreen();
 		HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
 		Spark.port(8080);
 
 		Spark.get("/hello", (req, res) -> "Hello World");
+//		
+//		System.out.println("Hoooola");
+//		
+//		Cliente unCliente = new Cliente();
+//		List<Dispositivo> dispositivos = new ArrayList<>();
+//		DispositivoFactory fabricaDeDispositivos = new DispositivoFactory();
+//		DispositivoInteligente dispositivo = fabricaDeDispositivos.aireAcondicionado2200();
+//		
+//		dispositivos.add(dispositivo);
+//		
+//		
+//		unCliente.setDispositivos(dispositivos);
+//		
+//		Spark.get("/elUSuario", (req,res) -> {
+//			return unCliente.getDispositivos();
+//			
+//		});
 		
-		System.out.println("Hoooola");
-		
-		Cliente unCliente = new Cliente();
-		List<Dispositivo> dispositivos = new ArrayList<>();
-		DispositivoFactory fabricaDeDispositivos = new DispositivoFactory();
-		DispositivoInteligente dispositivo = fabricaDeDispositivos.aireAcondicionado2200();
-		
-		dispositivos.add(dispositivo);
+//		//HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+//		Spark.get("/loginaccess", (req,res) -> {  
+//			return new ModelAndView(null, "Login.html");                     
+//		}, engine);
 		
 		
-		unCliente.setDispositivos(dispositivos);
-		
-		Spark.get("/elUSuario", (req,res) -> {
-			return unCliente.getDispositivos();
+		Spark.get("/loginaccess", (req,res) -> {
+			String nombreUsuario = req.queryParams("nombre");
+			String password = req.queryParams("password");
 			
-		});
-		
-		//HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
-		Spark.get("/formulario/access", (req,res) -> {  
-						
-			return new ModelAndView(null, "Login.html");                     
-		}, engine);
-		
-		
-		Spark.post("/usuario", (req,res) -> {
-			req.queryParams("nombre");
-			req.queryParams("password");
+//			EntityManagerHelper dbhelper = new EntityManagerHelper();
+//			EntityManager entityManager = PerThreadEntityManagers.getEntityManager();
+//			javax.persistence.Query query = entityManager.createQuery("SELECT * FROM USUARIOS WHERE usuario =:nombreUsuario");
 			
-			res.redirect("/home");
-			return null;
-		});	
+			System.out.println(query);
+			
+			
+			//res.redirect("/home");
+			return new ModelAndView(null, "Login.html"); 
+			
+		}, engine);	
 	}
 }
---
+++ src/test/java/Entrega4/ServidorPruebaLogin.java
@@ -0,0 +1,17 @@
+package Entrega4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Server.Server;
+
+public class ServidorPruebaLogin {
+
+	@Test
+	public void test() {
+		Server srv = new Server();
+		srv.loginUsuario();
+	}
+
+}
