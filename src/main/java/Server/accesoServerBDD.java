976c6144d7bbdb1c4e790160e7b58e9735f6efc5
Jey <jaeld94@gmail.com>
2018-11-07 20:47:41 -0300

    Update realizado en la entrega del TP

--- src/main/java/Server/accesoServerBDD.java
+++ src/main/java/Server/accesoServerBDD.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
@@ -15,19 +16,26 @@ import Usuario.Usuario;
 public class accesoServerBDD {
 
 	public Boolean controlLogin(String nombreUsuario, String password) {
-		EntityManagerHelper entityManagerHelper = new EntityManagerHelper();
-		EntityManager entityManager = entityManagerHelper.entityManager();		
+		//EntityManagerHelper entityManagerHelper = new EntityManagerHelper();
+		//EntityManager entityManager = entityManagerHelper.entityManager();		
 		
-		EntityTransaction transaccion = entityManager.getTransaction();
+		//EntityTransaction transaccion = entityManager.getTransaction();
 		//Usuario resultadoUsuarioQuery = entityManagerHelper.buscar(Usuario.class, new ImmutablePair<>("usuario",nombreUsuario));
 		
 		//Usuario resultadoUsuarioQuery  = (Usuario) entityManagerHelper.entityManager().createNativeQuery("select * from USUARIOS where usuario= '" +nombreUsuario+ "'");
 		
-		List<Usuario> result = (List<Usuario>) entityManagerHelper.entityManager().createNativeQuery("select u.* from USUARIOS u where usuario = " + '"' + nombreUsuario + '"' , Usuario.class).getResultList();
+		//List result = entityManagerHelper.entityManager().createNativeQuery("select u.* from USUARIOS u where usuario = " + '"' + nombreUsuario + '"').getResultList();
 		
-		Usuario resultado = result.get(0);
+		EntityManager entityManager = PerThreadEntityManagers.getEntityManager();
+		Query query = entityManager.createQuery("FROM Cliente");
+		//query.setParameter("nombreUsuario", nombreUsuario);
+		List list = query.getResultList();
 		
-		if (resultado.getPassword() == password) {
+		
+		Cliente resultado = (Cliente) list.get(0);
+		
+		//if (resultado.getPassword() == password) {
+		if ( resultado.getPassword() == password) {
 			return true;			
 		} else return false;
 		
